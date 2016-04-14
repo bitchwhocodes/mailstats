145e18b11d1ffc74ee1720ac05390b41dfd82a83
stacey <BitchWhoCodes@STACEYLs-MacBook-Pro.local>
2016-04-14 14:11:40 -0400

    Added new items and did visuals

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,9 @@
 
 
 $(document).ready(function(){
-    
-    console.log("doc is ready")
-   
     var angle = 0;
     var direction = 1;
   setInterval(function(){
-   
- 
     if(angle >=20){
         direction = -1;
     }else if(angle<= -20){
@@ -17,6 +12,4 @@ $(document).ready(function(){
        angle+=direction*5;
   $('.baehead').rotate(angle);
   },100);
-    
-    
 })
